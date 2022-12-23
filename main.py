782974b4d2df9a45b4d21d0816fc3493eab5e59e
Kiris <kiris2004@mail.ru>
2022-12-23 15:37:58 +0300

    Реализованно добавление индекса в таблицу на основании количества уже существующих записей

--- main.py
+++ main.py
@@ -1,26 +1,37 @@
 import sqlalchemy as sa
-import functions as func
+from functions import *
 import pandas as pd
 
 
-
-
-ticker = "TSLA" #Тикер акции
+tickers = ["TSLA","IBM","NVDA"] #Тикер акции
 apikey = open("api")#В файле "api" должен лежать ваш ключ от API
 engine = sa.create_engine("postgresql+psycopg2://admin:admin@localhost/final_project")
 
-
-insp = sa.inspect(engine)
-if (insp.has_table('stocks')):
-    ticker_search= pd.read_sql(f'''SELECT * FROM stocks WHERE "ticker" = '{ticker}' ''', engine)
-    if (len(ticker_search.index)>0):
-        print (ticker_search["datetime"])
-    else:
-        func.db_formation(ticker, engine, load_to_db = True)
+for ticker in tickers:
+    #Проверяем существование таблицы
+    insp = sa.inspect(engine)
+    if (insp.has_table('stocks')):
+        #Ищем строки по тикеру в нашей бд
+        ticker_search= pd.read_sql(f'''SELECT * FROM stocks WHERE "ticker" = '{ticker}' ''', engine)
+        #Если строк больше 0, 
+        #вытаскиваем последний элемент из нашей бд, 
+        #ищем в загруженном слепке с API этот элемент
+        #и загружаем все данные начиная со следующей строки в базу данных
+        if (len(ticker_search.index)>0):
+            last_line = ticker_search["datetime"].iloc[-1]
+            data = collect_data(ticker)
+            entry_point = data[data["datetime"]==str(last_line)].index[0]
+            slicer = data.iloc[entry_point:-1]
+            load_to_sql(slicer, engine)
+            
+        else:
+        #Если строк по этому тикеру нет - загружаем все данные. 
+            load_to_sql(collect_data(ticker), engine)
         
-         
-else:
-    print("Таблицы не существует")
-    func.db_formation(ticker, engine, load_to_db = True) 
+        
+            
+    else:
+        print("Таблицы не существует")
+        load_to_sql(collect_data(ticker), engine)
  
 
